dde5ac209553728aa710dd78dfd04b91a3bd38a4
Николай Стригачев <nikolai7832@yandex.ru>
2022-03-30 14:43:02 +0300

    Добавьте файлы проекта.

---
+++ Task2_ValueInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using DriveManager;// используем проверку доступности из Task1
+// код работает, но если доступ к файлу директории закрыт, то вес не узнать.
+namespace Volume
+{
+    public class VolumeInfo
+    {
+        
+        public static long GetDirSize(string path)
+        {
+            long size = 0;
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+                size += (new FileInfo(file)).Length;
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+                size += GetDirSize(dir);
+            return size;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу:");
+            string path = Console.ReadLine();
+            if (DirDeleter.IsAccesible(path) == true)
+            {
+                Console.WriteLine(GetDirSize(path) + " байт");
+            }
+            else
+            {
+                Main(args);
+            }
+
+        }
+    }
+}
